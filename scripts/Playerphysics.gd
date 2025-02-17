6e41ee6ec9574121b6309072729b5957935d77db
Upside0ut <165095372+Upside0ut@users.noreply.github.com>
2025-02-17 21:01:49 +1300

    player sprites and animation

--- scripts/Playerphysics.gd
+++ scripts/Playerphysics.gd
@@ -1,8 +1,9 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED = 200.0
+const JUMP_VELOCITY = -300.0
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -14,15 +15,30 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+	
+	if direction == 1:
+		animated_sprite_2d.flip_h = false
+	if direction == -1:
+		animated_sprite_2d.flip_h = true
+	
+	if velocity.y < 0:
+		animated_sprite_2d.play("jump")
+	elif velocity.y > 0:
+		animated_sprite_2d.play("fall")
+	elif direction != 0:
+		animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("idle")
+	
 	move_and_slide()
