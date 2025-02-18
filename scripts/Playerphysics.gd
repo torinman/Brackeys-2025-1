bf10eeb8eafdc5343c79d574464962f743081a51
torinman <torin@theblorenge.co.uk>
2025-02-18 15:46:52 +1300

    Add jar rigidbody and update physics

--- scripts/Playerphysics.gd
+++ scripts/Playerphysics.gd
@@ -1,30 +1,43 @@
 extends CharacterBody2D
 
 
-const SPEED = 200.0
-const JUMP_VELOCITY = -300.0
+const ACCEL = 40.0
+const JUMP_ACCEL = -30.0
+const FRIC = 0.87
+const AIR_FRIC = 0.89
+const V_JUMP_DELAY = 0.1
+const V_JUMP_START = -150
 @onready var animated_sprite_2d = $AnimatedSprite2D
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-
+var since_ground = V_JUMP_DELAY
 
 func _physics_process(delta):
+	since_ground += delta
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if is_on_floor():
+		since_ground = 0
+	
+	if Input.is_action_pressed("jump") and since_ground == 0:
+		velocity.y = V_JUMP_START
+	elif Input.is_action_pressed("jump") and since_ground <= V_JUMP_DELAY:
+		velocity.y += JUMP_ACCEL
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x += direction * ACCEL
+	if is_on_floor():
+		velocity.x *= FRIC  
+	else: 
+		velocity.x *= AIR_FRIC
 
 	
 	if direction == 1:
