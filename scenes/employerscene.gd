b00431e5d1760deb80cc309523c86343a5c4cf47
Upside0ut <165095372+Upside0ut@users.noreply.github.com>
2025-02-19 21:23:56 +1300

    start of diologue (unfinished)

---
+++ scenes/employerscene.gd
@@ -0,0 +1,13 @@
+extends AnimatedSprite2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if Input.is_action_just_pressed("dialogic_default_action"):
+		Dialogic.start("day1")
+		
