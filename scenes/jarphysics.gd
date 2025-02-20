dd72c5ec9784cc20603479cd274c1817fcd29a6d
torinman <torin@theblorenge.co.uk>
2025-02-20 19:10:32 +1300

    add levels

--- scenes/jarphysics.gd
+++ scenes/jarphysics.gd
@@ -1,10 +1,9 @@
-extends RigidBody2D
+extends Node2D
 
 @export var scentetoinstantiate: PackedScene
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass #queue_redraw()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -14,8 +13,8 @@ func _process(delta):
 
 func smash():
 	var new_scene = scentetoinstantiate.instantiate()
-	new_scene.position = get_node(".").position + Vector2(0, -5)
-	new_scene.rotation = get_node(".").rotation
+	new_scene.position = get_node("./Jar").position + get_node(".").position + Vector2(0, -5)
+	new_scene.rotation = get_node("./Jar").rotation + get_node(".").rotation
 	new_scene.get_children()[-1].emitting = true
 	add_sibling(new_scene)
 	queue_free()
