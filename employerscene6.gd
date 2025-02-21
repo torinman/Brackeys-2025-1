3562e7216f89620a289c8026a41a148f31947eed
Upside0ut <165095372+Upside0ut@users.noreply.github.com>
2025-02-21 20:13:13 +1300

    employer scenes done

--- employerscene6.gd
+++ employerscene6.gd
@@ -10,7 +10,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if once == 0:
-		Dialogic.start("day1")
+		Dialogic.start("day6")
 		once = 1
 
 
