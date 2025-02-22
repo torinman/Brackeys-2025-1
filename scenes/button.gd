46b153d44d94173cfe7c5de3458e058f0789a3ba
torinman <torin@theblorenge.co.uk>
2025-02-22 17:11:48 +1300

    buttonic stuff

--- scenes/button.gd
+++ scenes/button.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @onready var area = $Area2D
 @onready var sprite = $AnimatedSprite2D
+@export var groop = "Laserers"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -13,12 +14,12 @@ func _process(delta):
 
 
 func _on_area_2d_body_entered(body):
-	for node in get_parent().get_node("Laserers").get_children():
+	for node in get_parent().get_node(groop).get_children():
 		node.turn_on()
 	sprite.play("down")
 
 
 func _on_area_2d_body_exited(body):
 	sprite.play("up")
-	for node in get_parent().get_node("Laserers").get_children():
+	for node in get_parent().get_node(groop).get_children():
 		node.turn_off()
